88611701511829210cd27fb858d6e489be8cc7da
Dariusz Pławecki <Dariusz Pawecki>
2017-04-16 12:22:15 +0200

    init

---
+++ drawer.py
@@ -0,0 +1,39 @@
+import ctypes
+from sdl2 import *
+
+
+class Drawer:
+    def __init__(self, width, height, title):
+        self.width = width
+        self.height = height
+        self.title = title
+        self.window = ""
+        self.renderer = ""
+
+    def init_screen(self):
+        SDL_Init(SDL_INIT_VIDEO)
+        self.window = SDL_CreateWindow(self.title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, self.width,
+                                       self.height, SDL_WINDOW_SHOWN)
+        self.renderer = SDL_CreateRenderer(self.window, 0, 0)
+
+    def wait(self):
+        running = True
+        event = SDL_Event()
+        while running:
+            while SDL_PollEvent(ctypes.byref(event)) != 0:
+                if event.type == SDL_QUIT:
+                    running = False
+                    break
+        SDL_DestroyWindow(self.window)
+        SDL_Quit()
+
+    def draw_point(self, x, y, r=255, g=255, b=255):
+        SDL_SetRenderDrawColor(self.renderer, r, g, b, 255)
+        (x, y) = self.convert_coords(x, y)
+        SDL_RenderDrawPoint(self.renderer, x, y)
+
+    def refresh(self):
+        SDL_RenderPresent(self.renderer)
+
+    def convert_coords(self, x, y):
+        return round(self.width / 2 + x * 300), round(self.height / 2 - y * 300)
---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+from drawer import Drawer
+
+WIDTH = 1024
+HEIGHT = 768
+
+
+def main():
+    points = generate_points()
+
+    drawer = Drawer(WIDTH, HEIGHT, b"Mandelbrot")
+    drawer.init_screen()
+
+    count = 0
+    for point in points:
+        drawer.draw_point(point['r'], point['i'])
+        count += 1
+        if count % 1000 == 0:
+            print("{0} tys punktów".format(count/1000))
+
+    drawer.refresh()
+    drawer.wait()
+
+
+def is_point_in_set(c):
+    z = 0 + 0j
+    for i in range(25):
+        z = z**2 + c
+        if abs(z) > 2:
+            return False
+    return True
+
+
+def generate_points():
+    points = []
+    for x in range(-1000, 500, 1):
+        for y in range(-500, 500, 1):
+            point = dict()
+            c = complex(x/500, y/500)
+            point['r'] = c.real
+            point['i'] = c.imag
+            if is_point_in_set(c):
+                points.append(point)
+    return points
+
+if __name__ == "__main__":
+    sys.exit(main())
