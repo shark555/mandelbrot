9cbc36089ad0fd10a27b8fc31109c4b32f942f93
Dariusz Pławecki <Dariusz Pawecki>
2017-04-17 12:48:13 +0200

    Odcienie szarości

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 
     count = 0
     for point in points:
-        drawer.draw_point(point['r'], point['i'])
+        drawer.draw_point(point['r'], point['i'], point['color'], point['color'], point['color'])
         count += 1
         if count % 1000 == 0:
             print("{0} tys punktów".format(count/1000))
@@ -22,25 +22,36 @@ def main():
     drawer.wait()
 
 
-def is_point_in_set(c):
+def get_score_for_point(point):
+    threshold = 25
     z = 0 + 0j
-    for i in range(25):
-        z = z**2 + c
+    for i in range(threshold):
+        z = z**2 + point
         if abs(z) > 2:
-            return False
-    return True
+            return i
+    return threshold
 
 
 def generate_points():
+    x1 = -2
+    x2 = 2
+    y1 = -1.2
+    y2 = 1.2
+    precision = 300
+    color_step = 10
+
     points = []
-    for x in range(-600, 300, 1):
-        for y in range(-300, 300, 1):
+    for x in range(int(x1*precision), int(x2*precision), 1):
+        for y in range(int(y1*precision), int(y2*precision), 1):
             point = dict()
-            c = complex(x/300, y/300)
+            c = complex(x/precision, y/precision)
             point['r'] = c.real
             point['i'] = c.imag
-            if is_point_in_set(c):
+            score = get_score_for_point(c)
+            if score > 0:
+                point['color'] = score * color_step
                 points.append(point)
+
     return points
 
 if __name__ == "__main__":
